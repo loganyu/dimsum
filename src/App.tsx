82c54a7105b6783a5e59f9d157ef61c53559e046
Logan Yu <logan.yu@cadre.com>
2020-05-30 19:56:51 -0400

    organize navigation for dim sum

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
-function HomeScreen({ navigation }) {
+function ItemsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Items Screen</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
@@ -25,7 +25,7 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function DetailsScreen({ navigation }) {
+function ItemDetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
@@ -33,7 +33,7 @@ function DetailsScreen({ navigation }) {
         title="Go to Details... again"
         onPress={() => navigation.push('Details')}
       />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go to home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
       <Button
         title="Go back to first screen in stack"
@@ -43,6 +43,35 @@ function DetailsScreen({ navigation }) {
   );
 }
 
+function ItemNavigator({navigation}) {
+	return (
+		<Stack.Navigator initialRouteName="Items">
+			<Stack.Screen
+				name="Items"
+				component={ItemsScreen}
+				options={{
+					title: 'Dim Sum',
+					headerLeft: () => (
+						<Button
+						  onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+						  title="Menu"
+						  color="#00cc00"
+						/>
+					  ),
+				}}/>
+			<Stack.Screen name="Details" component={ItemDetailsScreen} />
+      	</Stack.Navigator>
+	)
+}
+
+function HomeScreen() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Home Screen</Text>
+		</View>
+	)
+}
+
 function HomeNavigator({navigation}) {
 	return (
 		<Stack.Navigator initialRouteName="Home">
@@ -50,70 +79,39 @@ function HomeNavigator({navigation}) {
 				name="Home"
 				component={HomeScreen}
 				options={{
-					title: 'My home',
+					title: 'Home',
 					headerLeft: () => (
 						<Button
 						  onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
-						  title="Info"
+						  title="Menu"
 						  color="#00cc00"
 						/>
 					  ),
 				}}/>
-			<Stack.Screen name="Details" component={DetailsScreen} />
+			<Stack.Screen name="Details" component={ItemDetailsScreen} />
       	</Stack.Navigator>
-	)
-}
 
-
-function CustomDrawerContent(props) {
-	return (
-	  <DrawerContentScrollView {...props}>
-		<DrawerItemList {...props} />
-		<DrawerItem
-		  label="Close drawer"
-		  onPress={() => props.navigation.dispatch(DrawerActions.closeDrawer())}
-		/>
-		<DrawerItem
-		  label="Toggle drawer"
-		  onPress={() => props.navigation.dispatch(DrawerActions.toggleDrawer())}
-		/>
-	  </DrawerContentScrollView>
 	);
 }
 
-function Feed({ navigation }) {
-	return (
-	  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-		<Text>Feed Screen</Text>
-		<Button
-		  title="Open drawer"
-		  onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-		/>
-		<Button
-		  title="Toggle drawer"
-		  onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
-		/>
-	  </View>
-	);
-  }
-  
-  function Notifications() {
-	return (
-	  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-		<Text>Notifications Screen</Text>
-	  </View>
-	);
-  }
+
 
 
 function MyDrawer() {
 	return (
-	  	<Drawer.Navigator initialRouteName="Feed" drawerContent={props => <CustomDrawerContent {...props} />}>
-			  <Drawer.Screen
-				  name="Feed" component={Feed}
-			  />
-      		<Drawer.Screen name="Notifications" component={Notifications} />
-			<Drawer.Screen name="Home Screen" component={HomeNavigator} />
+	  	<Drawer.Navigator initialRouteName="Home">
+      		<Drawer.Screen name="Home" component={HomeNavigator} />
+  	        <Drawer.Screen name="Favorites" component={ItemNavigator} />
+			<Drawer.Screen name="All Dim Sum" component={ItemNavigator} />
+			<Drawer.Screen name="Popular" component={ItemNavigator} />
+			<Drawer.Screen name="Teas" component={ItemNavigator} />
+			<Drawer.Screen name="Steamed" component={ItemNavigator} />
+			<Drawer.Screen name="Savory" component={ItemNavigator} />
+			<Drawer.Screen name="Buns" component={ItemNavigator} />
+			<Drawer.Screen name="Desserts" component={ItemNavigator} />
+			<Drawer.Screen name="Dumplings" component={ItemNavigator} />
+			<Drawer.Screen name="Rice Noodle Rolls" component={ItemNavigator} />
+			<Drawer.Screen name="Congee and Rice" component={ItemNavigator} />
 	  	</Drawer.Navigator>
 	);
 }
